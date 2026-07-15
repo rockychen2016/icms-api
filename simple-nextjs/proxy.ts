e3828afacae0b1945cfa8f63eb6e9108a8886a16
rock.chen <szygnet@qq.com>
2026-07-15 13:00:29 +0800

    update

--- simple-nextjs/proxy.ts
+++ simple-nextjs/proxy.ts
@@ -1,5 +1,3 @@
-import { ICMSServer } from "@icms-api/server";
-import { getServerHttpCookies, getServerHttpOpts, ICookies, IStorage, setServerHttpHeaders } from "iboot-http-client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function proxy(request: NextRequest, response:NextResponse) {
